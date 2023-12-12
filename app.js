98b296dba046ebd525e727f1738eb6af5f086ae3
Dersu <148701933+dersuprogg@users.noreply.github.com>
2023-12-12 13:38:32 +0500

    select all ui elements in project

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// SELECT UI ELEMENTS
+const againBtn = document.querySelector(".btn-again");
+const guessBtn = document.querySelector(".btn-guess");
+const headerTitle = document.querySelector("header h1");
+const board = document.querySelector(".board");
+const guessInput = document.querySelector(".guess");
+const msg = document.querySelector(".msg");
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".high-score");
