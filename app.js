525e77b68e3d8631d8a73694ba90a71dbe2905b3
Dersu <148701933+dersuprogg@users.noreply.github.com>
2023-12-12 14:21:56 +0500

    generate secret/random number between 1 and 20

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ const guessInput = document.querySelector(".guess");
 const msg = document.querySelector(".msg");
 const score = document.querySelector(".score");
 const highScore = document.querySelector(".high-score");
+
+// GENERATE RANDOM/SECRET NUMBER
+const secretNum = Math.floor(Math.random() * 20) + 1;
+board.textContent = secretNum;
